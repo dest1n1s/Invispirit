411eba82039f2c479406b57f36afa09d3189a202
Dest1n1 <648377563@qq.com>
2021-03-22 11:52:07 +0800

    删除无用文件

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -6,13 +6,9 @@ using UnityEngine.UI;
 public class HealthManager : MonoBehaviour
 {
     public int Hp;
-    [Header("UI")]
-    public Text HpText;
     // Start is called before the first frame update
     void Start()
     {
-        Hp = 100;
-        HpText = GameObject.Find("HpText").GetComponent<Text>();
     }
 
     // Update is called once per frame
