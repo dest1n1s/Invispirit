dest1n1s/Invispirit

commit e28e33147392b86b35673f4c672533044112858d

Restructure the network code
The part of PlayManager.cs that handles adding and removing player from
the list is moved to PlayerList.cs, because new features other than
handling player connection might be added in the future.

PlayManager.cs is then renamed to CustomNetworkManager.cs.

Assets/Scripts/Network/CustomNetworkManager.cs
@@ -1,0 +1,55 @@
+// <copyright file="CustomNetworkManager.cs" company="ECYSL">
+// Copyright (c) ECYSL. All rights reserved.
+// </copyright>
+
+namespace Network
+{
+    using Mirror;
+    using UnityEngine;
+
+    /// <summary>
+    /// Manages the connection and disconnection of clients and updates the list of players and the health bar stack
+    /// according to it.
+    /// <para>
+    /// A player will have two ID's. The player ID starts from 1 and is displayed in the UI. The net ID is the ID
+    /// assigned by <see cref="Mirror"/>.
+    /// </para>
+    /// </summary>
+    public class CustomNetworkManager : NetworkManager
+    {
+        [SerializeField]
+        private PlayerList playerList;
+
+        [SerializeField]
+        private HealthBarStack healthBarStack;
+
+        /// <summary>
+        /// Adds a new connected player to tbe player list and adds the player's health bar.
+        /// </summary>
+        /// <param name="conn">Connection from client.</param>
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            // Add the player to the player list
+            var player = this.playerList.CreateAndAddPlayer(this.playerPrefab);
+
+            // Add the player to the network server
+            NetworkServer.AddPlayerForConnection(conn, player);
+
+            this.healthBarStack.AddHealthBarForPlayer(player);
+        }
+
+        /// <summary>
+        /// Removes the player from the player list and removes the player's health bar when a client disconnects.
+        /// </summary>
+        /// <param name="conn">Connection from client.</param>
+        public override void OnServerDisconnect(NetworkConnection conn)
+        {
+            var player = conn.identity.gameObject;
+            this.playerList.RemovePlayerWithNetId(player);
+            this.healthBarStack.RemoveHealthBarForPlayer(player);
+
+            // Calling the base implementation is necessary
+            base.OnServerDisconnect(conn);
+        }
+    }
+}

Assets/Scripts/Network/PlayerList.cs
@@ -4,18 +4,83 @@
 
 namespace Network
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using Mirror;
+    using UnityEngine;
 
     /// <summary>
-    /// Maintains the list of players.
+    /// The list of all connected players.
     /// </summary>
-    /// <para>The class is necessary because <see cref="SyncDictionary{TKey,TValue}"/> is only available for subclasses
-    /// of <see cref="NetworkBehaviour"/>.</para>
     public class PlayerList : NetworkBehaviour
     {
+        private readonly SyncDictionary<uint, uint> playerIdForNetId = new SyncDictionary<uint, uint>();
+
         /// <summary>
-        /// Gets the dictionary that maps a player's net ID to that player's ID in game (player ID).
+        /// Returns the net ID's of the all players connected.
         /// </summary>
-        public SyncDictionary<uint, uint> PlayerIdForNetId { get; } = new SyncDictionary<uint, uint>();
+        /// <returns>the net ID's of the all players connected.</returns>
+        public IEnumerable<uint> NetIds()
+        {
+            return this.playerIdForNetId.Keys;
+        }
+
+        /// <summary>
+        /// Returns whether there is a player with the provide net ID in the list.
+        /// </summary>
+        /// <param name="playerNetId">the net ID to be checked.</param>
+        /// <returns>whether there is a player with the provide net ID in the list.</returns>
+        public bool ContainsNetId(uint playerNetId)
+        {
+            return this.playerIdForNetId.ContainsKey(playerNetId);
+        }
+
+        /// <summary>
+        /// Returns the player ID of the player with the provided net ID.
+        /// </summary>
+        /// <param name="playerNetId">the net ID to be searched.</param>
+        /// <returns>the player ID of the player with the provided net ID.</returns>
+        public uint PlayerIdForNetId(uint playerNetId)
+        {
+            return this.playerIdForNetId[playerNetId];
+        }
+
+        /// <summary>
+        /// Creates a new player object from a prefab, adds the player to the player list, and returns the player
+        /// object.
+        /// </summary>
+        /// <param name="playerPrefab">the prefab from which the player is to be created.</param>
+        /// <returns>the player object created from the method.</returns>
+        public GameObject CreateAndAddPlayer(GameObject playerPrefab)
+        {
+            var randomSpawnPosition = new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(-8.0f, 8.0f));
+            var player = Instantiate(playerPrefab, randomSpawnPosition, default);
+
+            // Set the player's player ID as the least possible unique int from 1
+            uint playerId = 1;
+            while (!this.playerIdForNetId.Values.Contains(playerId))
+            {
+                ++playerId;
+            }
+
+            // Set a unique object name for the player. This name is shown only in the debugger.
+            player.name = "Player " + playerId;
+
+            this.playerIdForNetId.Add(player.GetComponent<NetworkIdentity>().netId, playerId);
+            return player;
+        }
+
+        /// <summary>
+        /// Removes a player from the list.
+        /// </summary>
+        /// <param name="player">the player to be removed.</param>
+        public void RemovePlayerWithNetId(GameObject player)
+        {
+            var playerNetId = player.GetComponent<NetworkIdentity>().netId;
+            if (this.playerIdForNetId.ContainsKey(playerNetId))
+            {
+                this.playerIdForNetId.Remove(playerNetId);
+            }
+        }
     }
 }

Assets/Invispirit.unity
@@ -528,7 +528,7 @@ GameObject:
   - component: {fileID: 1738958770}
   - component: {fileID: 1738958772}
   m_Layer: 0
-  m_Name: PlayerManager
+  m_Name: NetworkManager
   m_TagString: Untagged
   m_Icon: {fileID: 0}
   m_NavMeshLayer: 0

Assets/Scripts/HealthBarStack.cs
@@ -27,7 +27,7 @@ public class HealthBarStack : NetworkBehaviour
     /// </summary>
     public override void OnStartClient()
     {
-        foreach (var playerNetId in this.playerList.PlayerIdForNetId.Keys)
+        foreach (var playerNetId in this.playerList.NetIds())
         {
             this.AddHealthBarForNetId(playerNetId);
         }
@@ -67,7 +67,8 @@ public class HealthBarStack : NetworkBehaviour
             return;
         }
 
-        foreach (var playerNetId in this.playerList.PlayerIdForNetId.Keys)
+        // Add the health bars for new players. Those who already exist will be skipped.
+        foreach (var playerNetId in this.playerList.NetIds())
         {
             this.AddHealthBarForNetId(playerNetId);
         }
@@ -100,8 +101,9 @@ public class HealthBarStack : NetworkBehaviour
     /// <param name="playerNetId">the net ID of the player.</param>
     private void AddHealthBarForNetId(uint playerNetId)
     {
-        // Do nothing if the net ID cannot be found. This is necessary because `playerList` might be not be synced yet.
-        if (!this.playerList.PlayerIdForNetId.ContainsKey(playerNetId))
+        // Do nothing if the net ID cannot be found.
+        // This additional check is necessary because `playerList`s on the server and client might not be synced yet.
+        if (!this.playerList.ContainsNetId(playerNetId))
         {
             return;
         }
@@ -112,7 +114,7 @@ public class HealthBarStack : NetworkBehaviour
             return;
         }
 
-        var playerId = this.playerList.PlayerIdForNetId[playerNetId];
+        var playerId = this.playerList.PlayerIdForNetId(playerNetId);
         var healthBarPosition = new Vector3(0, (-4 * playerId) + 4);
         var healthBar = new HealthBar(this.healthBarPrefab, $"Player{playerId}", this.transform, healthBarPosition);
         this.HealthBarForNetId.Add(playerNetId, healthBar);

Assets/Scripts/Network/PlayerManager.cs
@@ -1,85 +1,0 @@
-// <copyright file="PlayerManager.cs" company="ECYSL">
-// Copyright (c) ECYSL. All rights reserved.
-// </copyright>
-
-namespace Network
-{
-    using System.Linq;
-    using Mirror;
-    using UnityEngine;
-
-    /// <summary>
-    /// Manages the connection and disconnection of clients and updates the list of players and the health bar stack
-    /// according to it.
-    /// </summary>
-    public class PlayerManager : NetworkManager
-    {
-        private const uint MaxNumberOfPlayers = 100;
-
-        [SerializeField]
-        private PlayerList playerList;
-
-        [SerializeField]
-        private HealthBarStack healthBarStack;
-
-        private SyncDictionary<uint, uint> PlayerIdForNetId => this.playerList.PlayerIdForNetId;
-
-        /// <summary>
-        /// Called on the server when a client adds a new player with ClientScene.AddPlayer.
-        /// <para>The default implementation for this function creates a new player object from the playerPrefab.</para>
-        /// </summary>
-        /// <param name="conn">Connection from client.</param>
-        public override void OnServerAddPlayer(NetworkConnection conn)
-        {
-            // The player will spawn at on a random position.
-            var randomSpawnPosition = new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(-8.0f, 8.0f));
-
-            // Instantiate the player object
-            var player = Instantiate(this.playerPrefab, randomSpawnPosition, default);
-
-            // Set a unique name for the object for debugging
-            player.name = $"Player [connId={conn.connectionId}]";
-
-            // Add the player to the network server
-            NetworkServer.AddPlayerForConnection(conn, player);
-
-            // Set the player's player ID as the least possible unique int from 1 to MaxNumberOfPlayers.
-            uint playerId;
-            for (playerId = 1; playerId <= MaxNumberOfPlayers; playerId++)
-            {
-                if (!this.PlayerIdForNetId.Values.Contains(playerId))
-                {
-                    break;
-                }
-            }
-
-            this.PlayerIdForNetId.Add(player.GetComponent<NetworkIdentity>().netId, playerId);
-
-            // Add the health bar of the player
-            // this.healthBarStack = GameObject.Find("Canvas").GetComponent<HealthBarStack>();
-            this.healthBarStack.AddHealthBarForPlayer(player);
-        }
-
-        /// <summary>
-        /// Removes the player from the player list and removes the player's health bar when a client disconnects.
-        /// </summary>
-        /// <param name="conn">Connection from client.</param>
-        public override void OnServerDisconnect(NetworkConnection conn)
-        {
-            var playerObject = conn.identity.gameObject;
-            var netId = playerObject.GetComponent<NetworkIdentity>().netId;
-
-            // Removes the player from the player list
-            if (this.PlayerIdForNetId.ContainsKey(netId))
-            {
-                this.PlayerIdForNetId.Remove(netId);
-            }
-
-            // Removes the health bar of the player
-            this.healthBarStack.RemoveBarForPlayer(playerObject);
-
-            // Calling the base implementation is necessary
-            base.OnServerDisconnect(conn);
-        }
-    }
-}

commit cc6189fa5d22d20af73b29213e2c8049e4e5ba23

Fix network bugs introduced in e28e3314.

Assets/Scripts/Network/CustomNetworkManager.cs
@@ -29,12 +29,24 @@ namespace Network
         /// <param name="conn">Connection from client.</param>
         public override void OnServerAddPlayer(NetworkConnection conn)
         {
-            // Add the player to the player list
-            var player = this.playerList.CreateAndAddPlayer(this.playerPrefab);
+            // Generate randomly the spawn position
+            var randomSpawnPosition = new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(-8.0f, 8.0f));
 
-            // Add the player to the network server
+            // Instantiate the player object.
+            var player = Instantiate(this.playerPrefab, randomSpawnPosition, default);
+
+            // Set the player's player ID as the least possible unique int from 1
+            uint playerId = 1;
+            while (this.playerList.ContainsPlayerId(playerId))
+            {
+                ++playerId;
+            }
+
+            // Set a unique object name for the player. This name is shown only in the debugger.
+            player.name = "Player " + playerId;
+
             NetworkServer.AddPlayerForConnection(conn, player);
-
+            this.playerList.AddPlayer(player, playerId);
             this.healthBarStack.AddHealthBarForPlayer(player);
         }
 

Assets/Scripts/Network/PlayerList.cs
@@ -36,6 +36,16 @@ namespace Network
         }
 
         /// <summary>
+        /// Returns whether there is a player with the provided player ID in the list.
+        /// </summary>
+        /// <param name="playerId">the player ID to be checked.</param>
+        /// <returns>whether there is a player with the provided player ID in the list.</returns>
+        public bool ContainsPlayerId(uint playerId)
+        {
+            return this.playerIdForNetId.Values.Contains(playerId);
+        }
+
+        /// <summary>
         /// Returns the player ID of the player with the provided net ID.
         /// </summary>
         /// <param name="playerNetId">the net ID to be searched.</param>
@@ -46,28 +56,13 @@ namespace Network
         }
 
         /// <summary>
-        /// Creates a new player object from a prefab, adds the player to the player list, and returns the player
-        /// object.
+        /// Adds a player to the player list.
         /// </summary>
-        /// <param name="playerPrefab">the prefab from which the player is to be created.</param>
-        /// <returns>the player object created from the method.</returns>
-        public GameObject CreateAndAddPlayer(GameObject playerPrefab)
+        /// <param name="player">the player to be added.</param>
+        /// <param name="playerId">the player ID of the player.</param>
+        public void AddPlayer(GameObject player, uint playerId)
         {
-            var randomSpawnPosition = new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(-8.0f, 8.0f));
-            var player = Instantiate(playerPrefab, randomSpawnPosition, default);
-
-            // Set the player's player ID as the least possible unique int from 1
-            uint playerId = 1;
-            while (!this.playerIdForNetId.Values.Contains(playerId))
-            {
-                ++playerId;
-            }
-
-            // Set a unique object name for the player. This name is shown only in the debugger.
-            player.name = "Player " + playerId;
-
             this.playerIdForNetId.Add(player.GetComponent<NetworkIdentity>().netId, playerId);
-            return player;
         }
 
         /// <summary>